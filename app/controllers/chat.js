8e43b0f9af35ea8ca23503e2f8c7908e5e0e836f
Milton Quirino <miltonquirino.br@gmail.com>
2017-06-14 15:21:49 -0300

    add validation

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,17 @@
 module.exports.iniciaChat = function(application, req, res){
+
+  var dadosForm = req.body;
+
+  req.assert('apelido','Obrigatorio').notEmpty();
+  req.assert('apelido','minimo 3 maximo 15').len(3,15);
+
+  var errors = req.validationErrors();
+
+  if(errors){
+    res.render('index',{validacao:errors});
+    return;
+  }
+
+  console.log(dadosForm);
   res.render("chat");
 }
